a5780db885d8561ae7c8e4672ee24ef564f48c92
Jared Seefried <jared.seefried@yahoo.com>
2020-11-07 10:20:40 -0700

    Removed the window prompt and created a window to ask for the user Name and when the name is entered and the submit button is clicked, the chat message are will show

--- public/app.js
+++ public/app.js
@@ -1,44 +1,59 @@
-var socket = io(); // For Production
-// var socket = io('http://localhost:3000/') // For Testing LocalHost Server
-const messageContainer = document.getElementById("message-container")
+$(document).ready(function () {
+  // var socket = io(); // For Production
+  var socket = io('http://localhost:3000/') // For Testing LocalHost Server
+  const messageContainer = document.getElementById("message-container")
+  
+  const messageForm = document.getElementById("send-container")
+  const messageInput = document.getElementById("message-input")
+  
 
-const messageForm = document.getElementById("send-container")
-const messageInput = document.getElementById("message-input")
+  $(".chat-page").hide();
+  
+  $(".submit").click(function () {
+    const name = $(".username").val()
+    console.log(name)
+    $(".login-page").hide();
+    $(".chat-page").show()
+    appendMessage("You have joined")
+    socket.emit("new-user", name)
 
-let name = prompt("what is your name?")
-appendMessage("You have joined")
-socket.emit("new-user", name)
+    socket.on("user-connected", name => {
+      appendMessage(`${name} connected`)
+    })
+
+  })
+
+  socket.on("chat-message", data => {
+    appendMessage(`${data.name}: ${data.message}`)
+  })
 
 
-socket.on("chat-message", data => {
-  appendMessage(`${data.name}: ${data.message}`)
+
+  socket.on("user-disconnected", name => {
+    appendMessage(`${name} disconnected`)
+  })
+
+  messageForm.addEventListener("submit", error => {
+    error.preventDefault();
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit("send-chat-message", message)
+    messageInput.value = ""
+  })
+
+  function appendMessage(message) {
+    let messageElement = document.createElement("div")
+    messageElement.classList.add("newMessage")
+    messageElement.innerText = message
+
+    messageContainer.append(messageElement)
+
+    var objDiv = document.getElementById("message-container");
+    objDiv.scrollTop = objDiv.scrollHeight;
+  }
+
+
 })
 
-socket.on("user-connected", name => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on("user-disconnected", name => {
-  appendMessage(`${name} disconnected`)
-})
-
-messageForm.addEventListener("submit", error => {
-  error.preventDefault();
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit("send-chat-message", message)
-  messageInput.value = ""
-})
-
-function appendMessage(message) {
-  let messageElement = document.createElement("div")
-  messageElement.classList.add("newMessage")
-  messageElement.innerText = message
-
-  messageContainer.append(messageElement)
-
-  var objDiv = document.getElementById("message-container");
-  objDiv.scrollTop = objDiv.scrollHeight;
-}
 
 
